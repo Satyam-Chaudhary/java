6e358a039b7c68c01b8aeff42861d369259d6be3
Satyam Chaudhary <chaudharysatyam313@Satyams-MacBook-Air.local>
2022-11-26 03:48:16 +0530

    loops

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//for loop
+//for(initialisaton;condition;updation)
+
+public class Main {
+    public static void main(String[] args) {
+        // counter++ => counter = counter + 1
+        for(int counter = 0;counter< 11; counter++){
+            //System.out.println("hello ji");
+
+            System.out.println(counter);
+        }
+        //counter can be any variable like i or j
+        for(int i = 0;i<11; i++){
+            System.out.println(i);
+        }
+    }
+}
